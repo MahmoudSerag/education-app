b2033c5a1290d014fde0e38313a2930442ccf4ff
Mahmoud <sragmahmoud4@gmail.com>
2023-07-07 14:13:58 -0400

    feat: :sparkles: add timestamps property to chapter schema

--- src/database/schemas/chapter.schema.ts
+++ src/database/schemas/chapter.schema.ts
@@ -1,8 +1,11 @@
 import * as mongoose from 'mongoose';
 
-export const ChapterSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  image: { type: String, required: true },
-  academicYear: { type: Number, enum: [1, 2, 3], required: true },
-  description: { type: String, required: true },
-});
+export const ChapterSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    academicYear: { type: Number, enum: [1, 2, 3], required: true },
+    description: { type: String, required: true },
+  },
+  { timestamps: true },
+);
