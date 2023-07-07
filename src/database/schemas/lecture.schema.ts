78b03c02ec4bf93c6c06629614c5e180fcdc5f1b
Mahmoud <sragmahmoud4@gmail.com>
2023-07-07 13:43:02 -0400

    feat: :sparkles: add lecture schema

---
+++ src/database/schemas/lecture.schema.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+
+export const LectureSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  image: { type: String, required: true },
+  chapterId: {
+    type: mongoose.Schema.Types.ObjectId,
+    index: true,
+    required: true,
+    ref: 'Chapter',
+  },
+  academicYear: { type: Number, enum: [1, 2, 3], required: true },
+  price: { type: Number, required: true },
+  videoURLs: { type: [String], required: true },
+  pdfFiles: { type: [String] },
+});
