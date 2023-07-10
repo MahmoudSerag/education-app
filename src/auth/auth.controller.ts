5db9ecf6cd88cf1f83897b270e0fee9d5ebc1f56
Mahmoud <sragmahmoud4@gmail.com>
2023-07-10 18:01:16 -0400

    feat: :sparkles: add swagger to register api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,16 +8,43 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { registerDto } from './dto/register.dto';
 
 import { ErrorResponse } from 'src/helpers/errorHandling.helper';
+import {
+  apiInternalServerErrorResponse,
+  apiBadRequestResponse,
+  apiConflictResponse,
+} from 'src/helpers/swagger.helper';
 
+@ApiTags('Auth')
 @Controller('/api/v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiCreatedResponse({
+    status: 201,
+    description: 'User created, verify your account.',
+    schema: {
+      example: {
+        success: true,
+        statusCode: 201,
+        message: 'User created successfully',
+      },
+    },
+  })
+  @ApiConflictResponse(apiConflictResponse)
+  @ApiBadRequestResponse(apiBadRequestResponse)
+  @ApiInternalServerErrorResponse(apiInternalServerErrorResponse)
   @UsePipes(
     new ValidationPipe({
       exceptionFactory(error: object[]) {
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -7,38 +7,89 @@ import {
   IsEmail,
   Matches,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class registerDto {
+  @ApiProperty({
+    name: 'fullName',
+    type: String,
+    example: 'ElBon maher',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   fullName: string;
 
+  @ApiProperty({
+    name: 'phoneNumber',
+    type: String,
+    example: '01012345678',
+    required: true,
+  })
   @Matches(/^0[0-9]{10}$/, {
     message:
       'Invalid phone number format. Please provide a valid phone number starting with 0 followed by 10 digits.',
   })
   phoneNumber: string;
 
+  @ApiProperty({
+    name: 'email',
+    type: String,
+    example: '8g8dA@example.com',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    name: 'confirmedEmail',
+    type: String,
+    example: '8g8dA@example.com',
+    required: true,
+  })
   @IsNotEmpty()
   confirmedEmail: string;
 
+  @ApiProperty({
+    name: 'password',
+    type: String,
+    example: '123456AS#@',
+    required: true,
+  })
   @MinLength(6)
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    name: 'confirmedPassword',
+    type: String,
+    example: '123456AS#@',
+    required: true,
+  })
   @IsNotEmpty()
   confirmedPassword: string;
 
+  @ApiProperty({
+    name: 'academicYear',
+    type: Number,
+    enum: [1, 2, 3],
+    example: 2,
+    required: true,
+  })
   @IsNumber()
   @IsEnum([1, 2, 3])
   @IsNotEmpty()
   academicYear: number;
 
+  @ApiProperty({
+    name: 'sex',
+    type: String,
+    enum: ['ذكر', 'انثي'],
+    example: 'ذكر',
+    required: true,
+  })
   @IsEnum(['ذكر', 'انثي'])
   @IsNotEmpty()
   sex: string;
