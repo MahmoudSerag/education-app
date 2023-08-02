01076eed9307a769475ceb48ac13f9df43c9e8ff
Mahmoud <sragmahmoud4@gmail.com>
2023-08-02 12:40:05 -0400

    fix: :bug: fix role logic and rename middleware to RoleAuthMiddleware

--- src/middlewares/roleAuth.middleware.ts
+++ src/middlewares/roleAuth.middleware.ts
@@ -5,7 +5,7 @@ import { JWTService } from 'src/helpers/jwtService.helper';
 import { ErrorResponse } from 'src/helpers/errorHandlingService.helper';
 
 @Injectable()
-export class AdminMiddleware implements NestMiddleware {
+export class RoleAuthMiddleware implements NestMiddleware {
   constructor(
     private readonly jwtService: JWTService,
     private readonly errorResponse: ErrorResponse,
@@ -13,7 +13,7 @@ export class AdminMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     try {
       const decodedToken = this.jwtService.verifyJWT(req.cookies.accessToken);
-      if (decodedToken.role !== 'admin')
+      if (decodedToken.role === 'student')
         return this.errorResponse.handleError(res, 403, 'Forbidden');
     } catch (error) {
       return this.errorResponse.handleError(res, 500, error.message);
