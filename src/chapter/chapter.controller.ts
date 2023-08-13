50ebc276f32de261dd5086efa53bb30ac14f14fc
Mahmoud <sragmahmoud4@gmail.com>
2023-08-13 12:59:50 -0400

    feat: :sparkles: add swagger to createNewChapter api

--- src/chapter/chapter.controller.ts
+++ src/chapter/chapter.controller.ts
@@ -6,15 +6,47 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { ChapterService } from './chapter.service';
+import {
+  ApiProduces,
+  ApiTags,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiForbiddenResponse,
+  ApiUnauthorizedResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
 
 import { ErrorResponse } from 'src/helpers/errorHandlingService.helper';
-import { chapterDto } from './dto/chapter.dto';
+import {
+  apiBadRequestResponse,
+  apiForbiddenResponse,
+  apiUnauthorizedResponse,
+  apiInternalServerErrorResponse,
+} from 'src/helpers/swaggerService.helper';
 
+import { chapterDto } from './dto/chapter.dto';
+@ApiProduces('application/json')
+@ApiTags('Auth')
 @Controller('api/v1/chapters')
 export class ChapterController {
   constructor(private readonly chapterService: ChapterService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    status: 201,
+    description: 'User registration',
+    schema: {
+      example: {
+        success: true,
+        statusCode: 201,
+        message: 'User created successfully',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse(apiUnauthorizedResponse)
+  @ApiForbiddenResponse(apiForbiddenResponse)
+  @ApiBadRequestResponse(apiBadRequestResponse)
+  @ApiInternalServerErrorResponse(apiInternalServerErrorResponse)
   @UsePipes(
     new ValidationPipe({
       exceptionFactory(error: object[]) {
--- src/chapter/dto/chapter.dto.ts
+++ src/chapter/dto/chapter.dto.ts
@@ -5,12 +5,25 @@ import {
   IsString,
   Matches,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class chapterDto {
+  @ApiProperty({
+    name: 'title',
+    type: String,
+    example: 'الفيزياء الحديثة',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    name: 'academicYear',
+    type: Number,
+    example: 2,
+    required: true,
+  })
   @IsEnum([1, 2, 3], {
     message: 'academicYear must be one of the following values: 1, 2 or 3',
   })
@@ -18,13 +31,25 @@ export class chapterDto {
   @IsNotEmpty()
   academicYear: number;
 
+  @ApiProperty({
+    name: 'imageURL',
+    type: Number,
+    example: 'https://i.ibb.co/mSq8k9B/istockphoto-1023966316-1024x1024.jpg',
+    required: true,
+  })
   @Matches(/\bhttps?:\/\/\S+\.(?:jpg|jpeg|png|gif|bmp|svg|webp)\b/, {
     message: 'Image must be a valid URL',
   })
   @IsString()
   @IsNotEmpty()
-  image: string;
+  imageURL: string;
 
+  @ApiProperty({
+    name: 'description',
+    type: String,
+    example: 'النهاردة درس مهم موووت',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   description: string;
