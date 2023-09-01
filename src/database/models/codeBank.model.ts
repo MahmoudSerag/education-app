9b35eb908a28d785ede63dc71cb96ce316795ed0
Mahmoud <sragmahmoud4@gmail.com>
2023-08-31 21:17:18 -0400

    refactor: :recycle: improve code generation in createCodesBank function

--- src/database/models/codeBank.model.ts
+++ src/database/models/codeBank.model.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { CodeBankInterface } from 'src/code-bank/interface/codeBank.interface';
-import { CodeBankDto } from 'src/code-bank/dto/codeBank.dto';
 
 @Injectable()
 export class CodeBankModel {
@@ -12,19 +11,12 @@ export class CodeBankModel {
     private readonly codeBankModel: Model<CodeBankInterface>,
   ) {}
 
-  async createCodeBank(body: CodeBankDto): Promise<void> {
-    const codeBank = [];
-
-    for (let i = 0; i < body.numberOfCodes; i++) {
-      const min = Math.pow(10, 15);
-      const max = Math.pow(10, 16) - 1;
-      const randomCode = Math.floor(Math.random() * (max - min + 1)) + min;
-      codeBank.push({
-        code: randomCode,
-        price: body.codePrice,
-      });
-    }
-
-    await this.codeBankModel.insertMany(codeBank);
+  async createCodesBank(
+    generatedCodes: object[],
+  ): Promise<{ code: string; price: number }[]> {
+    return (await this.codeBankModel.insertMany(generatedCodes)).map((doc) => ({
+      code: `${doc.prefix}-${doc.code}`,
+      price: doc.price,
+    }));
   }
 }
