5407aa1bd2559c47287f9b687c3f166e5148f359
Mahmoud <sragmahmoud4@gmail.com>
2023-09-12 10:25:59 -0400

    feat: :sparkles: add swagger to "getUserProfile" API

---
+++ src/swagger/user/user.swagger.ts
@@ -0,0 +1,20 @@
+export const userProfile = {
+  name: 'User Profile',
+  description: 'Fetch user profile',
+  schema: {
+    example: {
+      success: true,
+      statusCode: 200,
+      message: 'User profile fetched successfully',
+      userProfile: {
+        _id: '64bdba13453cc737c5a34566',
+        email: 'sragmahmoud4@gmail.com',
+        fullName: 'محمود سراج اسماعيل',
+        academicYear: 2,
+        phoneNumber: '01064560430',
+        sex: 'ذكر',
+        wallet: 0,
+      },
+    },
+  },
+};
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,12 +2,29 @@ import { Controller, Get, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Response } from 'express';
 
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiProduces,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { userProfile } from 'src/swagger/user/user.swagger';
+import {
+  apiInternalServerErrorResponse,
+  apiUnauthorizedResponse,
+} from 'src/swagger/errors.swagger';
 
+@ApiProduces('application/json')
+@ApiTags('User')
 @Controller('api/v1/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('profile')
+  @ApiOkResponse(userProfile)
+  @ApiUnauthorizedResponse(apiUnauthorizedResponse)
+  @ApiInternalServerErrorResponse(apiInternalServerErrorResponse)
   getUserProfile(@Res({ passthrough: true }) res: Response): object {
     return this.userService.getUserProfile(res);
   }
